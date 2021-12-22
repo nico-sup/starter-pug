847d647656b702f686e8e3f67372ef7b7021bf7e
nico-sup <jeanne-nicolas@laposte.net>
2021-12-23 00:47:17 +0100

    make it better

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ app.set('views', './src/views');
 app.set('view engine', 'pug');
 app.use("/src", express.static('./src/'));
 app.get("/", (req, res) => {
-res.render("index")
+    res.render("index")
+})
+app.get("/resultWorkout", (req, res) => {
+    res.render("resultWorkout")
 })
 app.listen(process.env.PORT || 5000)
--- src/js/test.js
+++ src/js/test.js
@@ -1,21 +1,41 @@
 function check() {
     var input = document.querySelectorAll(".ifCheck");
+    var inputTrac = document.querySelectorAll(".ifCheckTrac");
     var total = 0;
-    for (var i = 0; i < input.length; i++) {
-      if (input[i].checked) {
+    var totalTrac = 0;
+    for (var i = 0; i < input.length && i < inputTrac.length; i++) {
+      if (input[i].checked && inputTrac[i].checked) {
         total += parseFloat(input[i].value);
+        document.getElementsByName("total")[0].value = total;
+        totalTrac += parseFloat(inputTrac[i].value);
+        document.getElementsByName("totalTrac").value = totalTrac;
+        document.getElementsByName("rangeDay")[0].value = total;
+        document.getElementsByName("rangeTrac")[0].value = totalTrac;
       }
     }
-    // document.getElementsByName("total")[0].value = total.toFixed(2);
-    // document.getElementById("helo").value = total.toFixed(2);
+}
+function setInputValue(){ 
+  document.getElementsByName("total")[0].value = localStorage.getItem('server');
+  document.getElementsByName("totalTrac").value = localStorage.getItem('trac');
+  document.getElementsByName("rangeDay")[0].value = localStorage.getItem('rangeDay');
+  document.getElementsByName("rangeTrac")[0].value = localStorage.getItem('rangeTrac');
 }
 
 function checkReload() {
   let boxes = document.getElementsByClassName('ifCheck').length;
+  let trac = document.getElementsByClassName('ifCheckTrac').length;
   function save() {	
-    for(let i = 1; i <= boxes; i++){
+    for(let i = 1; i <= boxes && i <= trac; i++){
       var checkbox = document.getElementById(String(i));
       localStorage.setItem("checkbox" + String(i), checkbox.checked);	
+      var input = document.getElementsByName("total")[0].value;
+      localStorage.setItem('server', input);
+      var inputTrac = document.getElementsByName("totalTrac").value;
+      localStorage.setItem('trac', inputTrac);
+      var rangeDay = document.getElementsByName("rangeDay")[0].value;
+      localStorage.setItem('rangeDay', rangeDay);
+      var rangeTrac = document.getElementsByName("rangeTrac")[0].value;
+      localStorage.setItem('rangeTrac', rangeTrac);
     }
   }
 
@@ -29,6 +49,3 @@ function checkReload() {
 
   window.addEventListener('change', save);
 }
-
-
-
