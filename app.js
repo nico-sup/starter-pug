5f1d449b16585bcf385f889784a212f63ee0783d
nico-sup <jeanne-nicolas@laposte.net>
2022-03-21 11:22:22 +0100

    evolution starter

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
+
 const app = express()   
-app.set('views', './src/views');
+
+app.set('views', './public/views');
 app.set('view engine', 'pug');
-app.use("/src", express.static('./src/'));
+app.use("/public", express.static('./public/'));
 app.get("/", (req, res) => {
     res.render("index")
 })
-// app.get("/resultWorkout", (req, res) => {
-//     res.render("resultWorkout")
+
+// app.get("/other-link", (req, res) => {
+//     res.render("other-link")
 // })
 
 app.listen(process.env.PORT || 5000)
