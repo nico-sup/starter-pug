987470829ed930fbe87e327e66ba187d37f0188e
nico-sup <jeanne-nicolas@laposte.net>
2022-03-25 08:27:52 +0100

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-
 const app = express()   
+
 
 app.set('views', './public/views');
 app.set('view engine', 'pug');
@@ -9,8 +9,11 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-// app.get("/other-link", (req, res) => {
-//     res.render("other-link")
-// })
+app.get("/post/:id", (req, res) => {
+    var id = req.params.id;
+    res.render("post", {
+        id: id
+    })
+})
 
 app.listen(process.env.PORT || 5000)
